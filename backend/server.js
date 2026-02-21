26e3dc8f92c1f96a4247841c3cf297d31b9f509c
kanchan2511 <kanchankumawat2509@gmail.com>
2026-02-21 16:09:49 +0530

    checking

--- backend/server.js
+++ backend/server.js
@@ -16,3 +16,7 @@ app.use("/api/observations", require("./routes/observationRoutes"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,() => console.log(`server running on ${PORT}`))
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
